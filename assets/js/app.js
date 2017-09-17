d5659c6dfd3d867b09e87d1b76daa249a48bec1e
root <root@localhost.localdomain>
2017-09-17 13:45:57 -0400

    App C&A

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,39 @@ $(function() {
 	var client = ZAFClient.init();
 	client.invoke('resize',{width: '100%',height: '465px'});
 	
+	var clientData = null;
+	
 	$.getJSON('http://www.mocky.io/v2/59bea3ee260000df00526189',function(data){
-		var source = $("#requester-template").html();
-		var template = Handlebars.compile(source);
-		var html = template(data.dados);
+		clientData   = data.dados;
 		
-		console.log(data.dados.beneficio[0])
+		var source   = $("#requester-template").html();
+		var template = Handlebars.compile(source);
+		var html     = template(clientData);
 		
 		$("#content").html(html);
-	})
+	});
+	
+	function getClientInfo(){
+		for(var x in clientData.informacoesCadastrais.emails){
+			var item = clientData.informacoesCadastrais.emails[x];
+			
+			if(item.email == clientData.dadosConsulta.emailConsulta){
+				return item;
+			}			
+		}
+	}
+	
+	Handlebars.registerHelper('email', function() {
+		return getClientInfo().email;
+	});
+	
+	Handlebars.registerHelper('optin', function() {
+		var optin = '';
+		
+		if(getClientInfo().optin == 'S'){
+			optin = 'checked';
+		}
+		
+		return optin;
+	});
 });
